14ee8a585623bedb27d9cf2d434139446742a93d
Ha Thu <tranhathu6989@gmail.com>
2023-04-05 23:05:48 +0200

    Update correction

--- exo16.php
+++ exo16.php
@@ -3,34 +3,48 @@
 class Waiter {
   const MAX_TABLES = 4;
 
-  private array $tables=[];
-  
-  public function addTable (Table $table): bool
-  {
-    if(count($this->tables) === self::MAX_TABLES) {
-      throw new Exception("Waiter can't have more than " . self::MAX_TABLES . " tables");
-    }
-    if(!in_array($table, $this->tables, true)) {
-      $this->tables[] = $table;
-      return true;
-    }
-    return false;
+  private string $name;
+  private array $tables = [];
+
+  public function __construct($name) {
+      $this->name = $name;
   }
   
-  public function removeTable(Table $table): bool
+  public function getName(): string
   {
-    $key = array_search($table, $this->tables, true);
-    if($key !== false) {
-      unset($this->tables[$key]);
-      return true;
+      return $this->name;
+  }
+
+  
+  public function setName(string $name): self
+  {
+      $this->name = $name;
+      return $this;
+  }
+
+  public function addTable(Table $table) {
+    if (count($this->tables) >= 4) {
+        throw new Exception('A waiter cannot be assigned to more than four tables.');
     }
-    return false;
+    if(!in_array($table, $this->tables)){
+        $this->tables[] = $table;
+    }
+}
+  
+  public function removeTable(Table $table) {
+    $key = array_search($table, $this->tables);
+    if ($key !== false) {
+        array_splice($this->tables, $key, 1);
+    } else {
+        throw new Exception('This table is not assigned to this waiter.');
+    }
   }
 
   public function setTables(array $tables): self
   {
     foreach($tables as $table) {
-      $this->addTable($table);
+      // $this->addTable($table);
+      array_push($this->tables, $table);
     }
     return $this;
   }
@@ -43,4 +57,40 @@ class Waiter {
 
 
 class Table {
+  private string $tableNumber;
+
+  public function __construct(string $tableNumber) 
+  {
+      $this->tableNumber = $tableNumber;
+  }
+
+  public function getTableNumber(): string
+  {
+      return $this->tableNumber;
+  }
+
+  public function setTableNumber(string $tableNumber): self
+  {
+      $this->tableNumber = $tableNumber;
+      return $this;
+  }
 }
+
+$waiter1 = new Waiter('Paul');
+$waiter2 = new Waiter('Marie');
+$table1 = new Table("table 1");
+$table2 = new Table("table 2");
+$table3 = new Table("table 3");
+$table4 = new Table("table 4");
+$table5 = new Table("table 5");
+
+$waiter1->addTable($table1);
+$waiter1->addTable($table2);
+$waiter1->addTable($table3);
+$waiter1->addTable($table4);
+// $waiter1->addTable($table5);
+var_dump($waiter1);
+
+echo "<br><br>";
+$waiter1->removeTable($table1);
+var_dump($waiter1);
--- exo18.php
+++ exo18.php
@@ -4,11 +4,14 @@ class Waiter {
   private array $tables=[];
   private string $name;
 
+  public function __construct($name) {
+    $this->name = $name;
+  }
+
   public function getName(): string
   {
       return $this->name;
   }
-
   
   public function setName(string $name): self
   {
@@ -16,25 +19,23 @@ class Waiter {
       return $this;
   }
 
-  public function addTable (Table $table): self
-  {
-    if(!array_search($table, $this->tables) && count($this->tables) < 4) {
-        array_push($this->tables, $table);
-        return $this;
-    } else {
-        throw new Exception('Maximum 4 tables for 1 waiter');
+  public function addTable (Table $table) {
+    if (count($this->tables) >= 4) {
+      throw new Exception('A waiter cannot be assigned to more than four tables.');
+    }
+    if(!array_search($table, $this->tables)) {
+         $this->tables[] = $table;
+        // array_push($this->tables, $table);
     }
   }
   
-  public function removeTable(Table $table): self
-  {
+  public function removeTable(Table $table) {
     $key = array_search($table, $this->tables);
-    if ($key !== false) {
+    if ($key !== false){
         array_splice($this->tables, $key, 1);
-        return $this;
-    } else {
-        throw new Exception('This table is not served by this waiter.');
-    }
+     } else {
+      throw new Exception('This table is not assigned to this waiter.');
+      }
   }
 
   public function setTables(array $tables): self
@@ -54,33 +55,56 @@ class Waiter {
 
 class Table {
   private array $waiters = [];
-  
-  public function getWaiters(): array
-  {
-    return $this->waiters;
+    
+  public function addWaiter(Waiter $waiter)
+  {   
+    $key = array_search($waiter, $this->waiters);
+    if(!$key){
+      $waiter->addTable($this); // thêm dòng này so vs Waiter
+      array_push($this->waiters, $waiter);
+    }
+    // if(!array_search($waiter, $this->waiters)){
+    //   // $this->waiters[] = $waiter;
+    //   array_push($this->waiters, $waiter);
+    //   $waiter->addTable($this);
+    // }
   }
-
+  
+  public function removeWaiter(Waiter $waiter)
+  {
+    $key = array_search($waiter, $this->waiters);
+    if($key !== false){
+      array_splice($this->waiters, $key, 1);
+      $waiter->removeTable($this); 
+    }
+    return $this;
+  }
+  
   public function setWaiters(array $waiters): self
   {
     foreach($waiters as $waiter) {
       $this->addWaiter($waiter);
     }
-   return $this;
+    return $this;
   }
 
-  public function addWaiter(Waiter $waiter): self
+  public function getWaiters(): array
   {
-      $this->waiters[] = $waiter;
-      $waiter->addTable($this); 
-      array_push($this->waiters, $waiter);
-      return $this;
-  }
-
-  public function removeWaiter(Waiter $waiter): self
-  {
-      $key = array_search($waiter, $this->waiters);
-      unset($this->waiters[$key]);
-      $waiter->removeTable($this); 
-      return $this;
+    return $this->waiters;
   }
 }
+
+$waiter1 = new Waiter('Paul');
+$waiter2 = new Waiter('Marie');
+$waiter3 = new Waiter('Sara');
+
+$table1 = new Table();
+$table1->addWaiter($waiter1);
+$table1->addWaiter($waiter2);
+$table1->addWaiter($waiter3);
+var_dump($table1);
+
+echo"<br> <br>";
+$table1->removeWaiter($waiter1);
+var_dump($table1);
+
